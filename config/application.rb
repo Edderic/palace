54be2272a3518a13d56f8116b1d2b45ddf9afc75
edderic <edderic@gmail.com>
2014-01-26 10:17:55 -0500

    Added rails env != production line

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(:default, Rails.env)
 
 path = File.expand_path('../application.yml', __FILE__)
 
-if File.exists?(path)
+if ENV['RAILS'] == 'production'|| File.exists?(path)
   config = YAML.load(File.read(path))
   config.merge! config.fetch(Rails.env, {})
   config.each do |key,value|
